84bab8da2f543f0878853e91b79a361b34e164d1
Stuart Thiel <stuart.thiel@gmail.com>
2025-03-18 12:52:25 -0300

    Added doors

---
+++ Door.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Door extends Obstacle {
+	private Key key;
+	
+	@Override
+	public String toString() {
+		return "+";
+	}
+	
+	public Door(Key key) {
+		this.key = key;
+	}
+	
+	public boolean canSolve(Player p) {
+		
+		for(Key k: p.getKeys()) {
+			if(key.equals(k)) {
+				return true;
+			}
+		}
+		
+		Map.sp.play(SoundPlayer.SOUNDS.BONK);
+		Map.messages.add("The door is locked and you bump into it. It appears to have keyhole for " + key);
+		return false;
+
+	}
+	
+	
+}
---
+++ Obstacle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public abstract class Obstacle {
+	
+	
+	public abstract boolean canSolve(Player p);
+	
+	
+}
